9aa0fb1b04867c55e1322013c73489a399e64669
WebFan37 <118756551+WebFan37@users.noreply.github.com>
2025-03-04 12:52:28 -0800

    Finir le triage

--- src/composants/ListeProduits.jsx
+++ src/composants/ListeProduits.jsx
@@ -2,24 +2,62 @@ import './ListeProduits.scss';
 import teeshirts from "../data/teeshirts.json";
 import Produit from "./Produit.jsx";
 
-function ListeProduits({panier, setPanier}){
+function ListeProduits({panier, setPanier, filtre, tri}){
+
+    // console.log(filtre);
+
+    //Filter and triage the products
+    const produitFiltreEtTrie = teeshirts.filter(({categorie}) => {
+       return filtre == "tous" || categorie == filtre; 
+    }).sort(functionComparison[tri]);
 
     return (
-<article className="principal liste-produits">
+    <article className="principal liste-produits">
 
-{teeshirts.map(
-    teeshirt => (
-       <Produit {...teeshirt}
-       panier={panier} setPanier={setPanier}
-       />
+    {produitFiltreEtTrie.map(
+        teeshirt => (
+        <Produit {...teeshirt} panier={panier} setPanier={setPanier}
+        />
+        )
+
+    )}
+
+    </article>
+
     )
 
-)}
+}
 
-</article>
+//Determine the order of the products
+//Each option in the dropdown menu has a corresponding function
+//The function is called when the option is selected
+const functionComparison = {
 
-    )
 
+    //=========/IMPORTANT/=========//
+    //The name of the function is the same as the value of the option in the dropdown menu
+    //Same as the value=xxxx in the option tag
+
+
+    //First function is a random function
+    alea: function(element1, element2) {return Math.random() - 0.5},
+
+    //Order it by price
+    //Compare the price of the first element with the second element
+    prixASC: (element1, element2) => element1.prix - element2.prix,
+
+    //Compare the price of the second element with the first element
+    prixDESC: (element1, element2) => element2.prix - element1.prix,
+
+    //Name: use the localeCompare function to compare the two names
+    nomASC: (element1, element2) => element1.nom.localeCompare(element2.nom, "fr"),
+    nomDESC: (element1, element2) => element2.nom.localeCompare(element1.nom, "fr"),
+
+    //Compare the number of sales of the first element with the second element
+    dacDESC: (element1, element2) => element2.dac.localeCompare(element1.dac, "fr"),
+
+    //Compare the number of sales of the second element with the first element
+    ventesDESC: (element1, element2) => element2.ventes - element1.ventes
 }
 
 export default ListeProduits;
--- src/composants/Principal.jsx
+++ src/composants/Principal.jsx
@@ -2,9 +2,17 @@ import './Principal.scss';
 
 
 import ListeProduits from './ListeProduits.jsx';
+import { useState } from 'react';
 
 function Principal({panier, setPanier}) {
-    
+
+  //Filtre
+  const [filtre, setFiltre ] = useState("tous");
+    // console.log(filtre);
+
+    //Triage
+    const [tri, setTri ] = useState("alea");
+
   return (
     <main className="Principal page-produits page-teeshirts">
       <article className="amorce">
@@ -13,32 +21,35 @@ function Principal({panier, setPanier}) {
           <form action="" method="get">
                 <div className="filtre">
                 <label htmlFor="filtre">Filtrer par : </label>
-                <select name="filtre" id="filtre">
+                <select name="filtre" id="filtre" value={filtre} onChange={
+                  (event)=>{setFiltre(event.target.value)} 
+
+                }>
                     <option value="tous">Tous les produits</option>
 
-                    <option value="1">Animaux</option>
-                    <option value="2">Inusité</option>
-                    <option value="4">Nature</option>
-                    <option value="3">Sport</option>
+                    <option value="animaux">Animaux</option>
+                    <option value="inusite">Inusité</option>
+                    <option value="nature">Nature</option>
+                    <option value="sport">Sport</option>
                 </select>
                 </div>
                 <div className="tri">
                     <label htmlFor="tri">Trier par : </label>
-                    <select name="tri" id="tri">
-                        <option value="RAND()">Aléatoire</option>
-                        <option value="prix ASC">Prix / ascendant</option>
-                        <option value="prix DESC">Prix / descendant</option>
-                        <option value="nom ASC">Nom / ascendant</option>
-                        <option value="nom DESC">Nom / descendant</option>
-                        <option value="dac DESC">Nouveauté</option>
-                        <option value="ventes DESC">Meilleur vendeur</option>
+                    <select name="tri" id="tri" value={tri} onChange={(event)=> {setTri(event.target.value)}}>
+                        <option value="alea">Aléatoire</option>
+                        <option value="prixASC">Prix / ascendant</option>
+                        <option value="prixDESC">Prix / descendant</option>
+                        <option value="nomASC">Nom / ascendant</option>
+                        <option value="nomDESC">Nom / descendant</option>
+                        <option value="dacDESC">Nouveauté</option>
+                        <option value="ventesDESC">Meilleur vendeur</option>
                     </select>
                 </div>
           </form>
         </section>
     </article>
 
-  <ListeProduits panier={panier} setPanier={setPanier}/>
+  <ListeProduits panier={panier} setPanier={setPanier} filtre={filtre} tri={tri}/>
 
   </main>
   )
